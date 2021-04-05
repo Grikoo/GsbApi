ea2f3f2eeae978cd1d969beeb496ef97bc97f376
Grikoo <ricky.issaad@epsi.fr>
2021-04-05 12:12:53 +0200

    Secure avec token sur rapport_visite

--- rapport_visite/create.php
+++ rapport_visite/create.php
@@ -8,6 +8,17 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include_once '../config/database.php';
 include_once '../objects/rapport_visite.php';
+include_once '../config/session.php';
+
+//debut token
+$user = filter_input(INPUT_POST,'user',FILTER_SANITIZE_SPECIAL_CHARS);
+$pwd = filter_input(INPUT_POST,'pwd',FILTER_SANITIZE_SPECIAL_CHARS);
+$token = filter_input(INPUT_POST,'token',FILTER_SANITIZE_SPECIAL_CHARS);
+$timer = filter_input(INPUT_POST,'valid',FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+$login = new Session();
+$login->login($user, $pwd, $token ,$timer);
 
 $database = new Database();
 $db = $database->getConnection();
